188ad04ce0eacfce1a8fa2fdcab0a4d775adc21d
Li-Wen Hsu <lwhsu@lwhsu.org>
2014-02-22 04:12:33 +0800

    Should be 192.168.56.105

--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ sleep 60
 echo "${VM_NAME} started"
 
 echo "Begin test"
-ssh -o StrictHostKeyChecking=no -o ConnectTimeout=1 -i /usr/local/jenkins/.ssh/id_rsa 192.168.56.100 'uname -a'
-ssh -o StrictHostKeyChecking=no -o ConnectTimeout=1 -i /usr/local/jenkins/.ssh/id_rsa 192.168.56.100 'dmesg'
+ssh -o StrictHostKeyChecking=no -o ConnectTimeout=1 -i /usr/local/jenkins/.ssh/id_rsa 192.168.56.105 'uname -a'
+ssh -o StrictHostKeyChecking=no -o ConnectTimeout=1 -i /usr/local/jenkins/.ssh/id_rsa 192.168.56.105 'dmesg'
 echo "Test Over"
 
 VBoxManage controlvm ${VM_NAME} acpipowerbutton
